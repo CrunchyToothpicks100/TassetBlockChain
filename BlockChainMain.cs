c26f702945d8b374d28afbeeabf170d160f8c053
CrunchyToothpicks100 <wolfywoofmc@gmail.com>
2026-04-30 21:29:31 -0500

    Untrack backup file, tweak terminal output

--- BlockChainMain.cs
+++ BlockChainMain.cs
@@ -8,24 +8,33 @@ namespace BlockChain
     {
         const string INPUT_FILE_NAME = @"StudentBlockTasset.txt";
         const string OUTPUT_FILE_NAME = @"Success.csv";
+        const string BACKUP_FILE_NAME = @"Success_backup.csv";
+
         const int BLOCKLINECOUNT = 4;
         static int difficulty = 9; // number of zeros
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Creating a new block from data file.\n");
+            Console.WriteLine("Creating a new block from data file.");
             StreamReader fileIn = File.OpenText(INPUT_FILE_NAME);
 
             string blockLines = ReadABlockOfData(fileIn);
             Block block = new Block(blockLines);
 
-            Console.WriteLine($"Mining block...\n");
+            Console.WriteLine($"Mining block... \nCtrl+C to exit \n");
 
             using var cts = new CancellationTokenSource();
-            Console.CancelKeyPress += (_, e) => { 
+
+            // When Ctrl+C is pressed...
+            Console.CancelKeyPress += (_, e) => {
+                // Use cancellation token to signal the mining process to stop
                 e.Cancel = true; 
                 cts.Cancel(); 
                 Console.WriteLine("Tasks cancelled safely.");
+
+                // This method creates and/or opens the backup file and copies the output to it
+                File.AppendAllText(BACKUP_FILE_NAME, File.ReadAllText(OUTPUT_FILE_NAME));
+                Console.WriteLine($"Backup of '{OUTPUT_FILE_NAME}' created as '{BACKUP_FILE_NAME}'.");
             };
 
             using (StreamWriter fileOut = new StreamWriter(OUTPUT_FILE_NAME, true)) // append mode = true
@@ -42,7 +51,7 @@ namespace BlockChain
                     },
                     stopToken: cts.Token
                 );
-            }
+            } // fileOut is automatically closed here
         }
 
         static string ReadABlockOfData(StreamReader fileIn)
